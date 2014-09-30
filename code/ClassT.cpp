9dfc1dca61cbe882cadb4b6201432bf402fc00e0
fuxi <1234@gmail.com>
2014-09-30 15:50:48 +0800

    重新理解友元类的含义 派生类可以与基类定义成友元

--- code/ClassT.cpp
+++ code/ClassT.cpp
@@ -3,15 +3,19 @@
 //using namespace std;
 int main()
   {
-      TestClass *t1;
+      TestClass *t1;//不管什么类型的指针在声明的时候最好初始化
+      TestClass  a1;
+      a1.test();
+      cout<<"TestClass的str默认字符串为:"<<a1.str<<endl;
+       a1.str=("天地无极 万里追踪");
+       t1=&a1;
+       t1->str=("鬼斧神工");// 类的成员变量不能通过未初始化的类指针进行赋值 
+      cout<<t1->str<<endl;
       HellCode  h1;
-       t1->test();
-      int b=t1->hell(3,4);
-      cout<<b<<endl;
-      cout<<t1->hell(5,5)<<endl;
-      h1.hello();
-    // t1->hello();
-     cout<<h1.jisu(2)<<endl;
-      HellCode  h2;
+      h1.test();
+     HellCode *b1=&h1;
+     h1.hello();
+     // t1=&h1;
+   //  t1->hello();就算基类指针对派生类对象进行引用也无法访问派生类的函数
       return 0;
   }
--- code/TestClass.h
+++ code/TestClass.h
@@ -4,22 +4,38 @@
   {
       friend class HellCode;  //将hellCode这个类 声明为友元类  
       public:
-           // string str("1");  //类的数据成员在定义的时候不能初始化???
-             string str; 
+              TestClass();
+             ~TestClass();
+           // string str("1");  //类的数据成员在定义的时候不能初始化
+             string str;
              void test();
+      private: 
+              int age;
+              string ageStr;
              int  hell(int a,int b);
+   
   };
 
-  string str("类的成员变量在定义的时候不能初始化");
+//:  string str("类的成员变量在定义的时候不能初始化");
+TestClass::TestClass():age(91),str("尚未初始化"),ageStr("这是TestClass的agestr") 
+          {
+        
+          }
+
+TestClass::~TestClass()
+          {
+      
+          }
 void  TestClass::test()
-{
-  cout<<"hello world"<<endl;
-}
+    {
+      
+         cout<<"hello world::"<<age<<endl;
+    }
 int TestClass::hell(int a,int b)
 {
    return a*b;
 }
-class HellCode
+class HellCode:public TestClass
    {
        
      public:
@@ -37,10 +53,10 @@ HellCode::~HellCode()
        cout<<"这个Hello code 类将会被销毁"<<endl;
  }
 void HellCode::hello()
- {
-       cout<<"看来友元声明成功了 "<<str<<endl;
-       cout<<"这个嘛 不清楚" <<endl; 
- }
+    {
+        cout<<"这里是Hellocode派生类 "<<ageStr<<endl;
+        cout<<"Hellcode::"<<"hello world" <<endl; 
+    }
 int  HellCode::jisu(int h)
   {
        
